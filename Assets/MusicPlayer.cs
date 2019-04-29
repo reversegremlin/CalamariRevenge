073eabb84b20c4dfca37561d96b962e2218034b3
Nadia Morris <reversegremlin@gmail.com>
2019-04-29 17:19:56 -0400

    Added Waypoints, Skybox, Music

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+
+    }
+
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
