4a727c4fe8dec309e2767d95d59ac23f1580a8d5
Nadia Morris <reversegremlin@gmail.com>
2019-04-30 11:44:08 -0400

    Added clamp to ship movement to keep it on screen

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class Player : MonoBehaviour
+{
+
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 10f;
+    [SerializeField] float xClampValue = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        //take the ships x position
+        // - 5 to +5
+        //work out a new x position
+        float rawNewXPosition = transform.localPosition.x + xOffset;
+        float newXPosition = Mathf.Clamp(rawNewXPosition, -xClampValue, xClampValue);
+        transform.localPosition = new Vector3(newXPosition, transform.localPosition.y, transform.localPosition.z);
+
+    }
+}
