c23824daa139c81179d44792fafbb703fd7a7ab1
Nadia Morris <reversegremlin@gmail.com>
2019-04-30 11:50:31 -0400

    Vertical movement and clamping

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,8 +7,10 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
 
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 10f;
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 10f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 10f;
     [SerializeField] float xClampValue = 5f;
+    [SerializeField] float yClampValue = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +29,15 @@ public class Player : MonoBehaviour
         float rawNewXPosition = transform.localPosition.x + xOffset;
         float newXPosition = Mathf.Clamp(rawNewXPosition, -xClampValue, xClampValue);
         transform.localPosition = new Vector3(newXPosition, transform.localPosition.y, transform.localPosition.z);
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        //take the ships x position
+        // - 5 to +5
+        //work out a new x position
+        float rawNewYPosition = transform.localPosition.y + yOffset;
+        float newYPosition = Mathf.Clamp(rawNewYPosition, -yClampValue, yClampValue);
+        transform.localPosition = new Vector3(transform.localPosition.x, newYPosition, transform.localPosition.z);
 
     }
 }
